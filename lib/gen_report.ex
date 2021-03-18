1713e75312d567b37c4519190163a5c13c2e4912
João Pedro Alves <jotalmeida007@hotmail.com>
2021-03-18 11:25:38 -0300

    Add kernel function
    Change map.put to put_in

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -31,7 +31,7 @@ defmodule GenReport do
   defp get_hours([name, hours, _day, _month, _year], %{
          "all_hours" => all_hours
        }) do
-    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+    all_hours = put_in(all_hours[name], all_hours[name] + hours)
 
     %{
       "all_hours" => all_hours
