636ebb91f3e9bf73db9f810e67bbaf8530dbc108
João Pedro Alves <jotalmeida007@hotmail.com>
2021-03-18 10:35:44 -0300

    Resolve get_hours

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,5 +1,5 @@
 defmodule GenReport do
-  alias GenReport.{Parser, GetHours}
+  alias GenReport.Parser
 
   @file_name "gen_report.csv"
 
@@ -20,11 +20,21 @@ defmodule GenReport do
     @file_name
     |> Parser.parse_file()
     |> Enum.reduce(hours_acc(), fn row, acc ->
-      GetHours.get_hours(row, acc)
+      get_hours(row, acc)
     end)
   end
 
   defp hours_acc(), do: %{"all_hours" => %{}} |> Map.put("all_hours", acc_name_map_gen(0))
 
   defp acc_name_map_gen(value), do: Enum.into(@names, %{}, &{&1, value})
+
+  defp get_hours([name, hours, _day, _month, _year], %{
+         "all_hours" => all_hours
+       }) do
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+
+    %{
+      "all_hours" => all_hours
+    }
+  end
 end
